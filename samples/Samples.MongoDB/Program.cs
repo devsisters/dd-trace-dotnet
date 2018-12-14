f2b4c79b52e6e985b46317301ddef9a327c2d29c
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2018-12-14 13:35:21 -0500

    test on MongoDB.Driver 2.5.0 first

--- samples/Samples.MongoDB/Program.cs
+++ samples/Samples.MongoDB/Program.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Linq;
+using Datadog.Trace.ClrProfiler;
 using MongoDB.Bson;
 using MongoDB.Driver;
 
@@ -9,6 +9,9 @@ namespace Samples.MongoDB
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine($"Profiler attached: {Instrumentation.ProfilerAttached}");
+            Console.WriteLine($"Platform: {(Environment.Is64BitProcess ? "x64" : "x32")}");
+
             var allFilter = new BsonDocument();
 
             var newDocument = new BsonDocument
@@ -31,13 +34,15 @@ namespace Samples.MongoDB
             collection.DeleteMany(allFilter);
             collection.InsertOne(newDocument);
 
-            var count = collection.CountDocuments(new BsonDocument());
-            Console.WriteLine(count);
+            var count = collection.Count(new BsonDocument());
+            Console.WriteLine($"Documents: {count}");
 
             var allDocuments = collection.Find(allFilter).ToList();
 
             Console.WriteLine(newDocument == allDocuments[0]);
-            Console.WriteLine(Environment.Is64BitProcess ? "64" : "32");
+
+            Console.WriteLine("Press [ENTER] to exit...");
+            Console.ReadLine();
         }
     }
 }
